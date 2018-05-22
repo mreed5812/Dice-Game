c8c6ba982b2138b8f386d07a0db4d9da7a15106a
Mike Reed <mreed5812@gmail.com>
2018-05-21 22:56:44 -0500

    Added state variable to start/stop gameplay when game over or new game

--- app.js
+++ app.js
@@ -13,20 +13,19 @@ var scores, roundScore, activePlayer, gamePlaying;
 
 init();
 
-
 document.querySelector('.btn-roll').addEventListener('click', function(){
-
-		// Random number
+	if(gamePlaying){
+		// 1. Random number
 
 		var dice = Math.floor(Math.random() * 6) + 1;
 
-		// Display the result
+		// 2. Display the result
 		var diceDOM = document.querySelector('.dice')
 		diceDOM.style.display = 'block';
 		diceDOM.src = 'dice-' + dice + '.png';
 
 
-		// Update the round score only IF the rolled number was not a 1
+		// 3. Update the round score only IF the rolled number was not a 1
 		if (dice !== 1) {
 			//Add score
 			roundScore += dice;
@@ -35,42 +34,42 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 			//Next player
 			nextPlayer();
 		}
+	}	
 });
 
 document.querySelector('.btn-hold').addEventListener('click',function(){
-	// Add CURRENT score to GLOBAL score
-	scores[activePlayer] += roundScore;
+	if(gamePlaying){
+		// Add CURRENT score to GLOBAL score
+		scores[activePlayer] += roundScore;
 
-	// Update the UI
-	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
-	// Check if player won the game
-	if(scores[activePlayer] >= 100) {
-		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
-		document.querySelector('.dice').style.display = 'none';
-		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-	} else{
-		//Next player
-		nextPlayer();
+		// Update the UI
+		document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+		// Check if player won the game
+		if(scores[activePlayer] >= 100) {
+			document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+			document.querySelector('.dice').style.display = 'none';
+			document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+			document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+			gamePlaying = false;
+		} else{
+			//Next player
+			nextPlayer();
+		}
 	}
-
-
 })
-
-
 
 function nextPlayer(){
 	//Next player
-			activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-			roundScore = 0;
+	activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+	roundScore = 0;
 
-			document.getElementById('current-0').textContent = '0';
-			document.getElementById('current-1').textContent = '0';
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
 
-			document.querySelector('.player-0-panel').classList.toggle('active');
-			document.querySelector('.player-1-panel').classList.toggle('active');
+	document.querySelector('.player-0-panel').classList.toggle('active');
+	document.querySelector('.player-1-panel').classList.toggle('active');
 
-			document.querySelector('.dice').style.display = 'none';
+	document.querySelector('.dice').style.display = 'none';
 }
 
 document.querySelector('.btn-new').addEventListener('click', init);
@@ -80,7 +79,7 @@ function init(){
 	scores = [0,0];
 	activePlayer = 0;
 	roundScore = 0;
-
+	gamePlaying = true;
 	document.querySelector('.dice').style.display = 'none';
 
 	document.getElementById('score-0').textContent = '0';
