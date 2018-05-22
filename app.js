1313fb64c75ba461fc9f60e8b051c9a875bd645e
Mike Reed <mreed5812@gmail.com>
2018-05-21 20:19:24 -0500

    Added game functionality

--- app.js
+++ app.js
@@ -43,6 +43,37 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 			document.querySelector('#current-' + activePlayer).textContent = roundScore;
 		} else{
 			//Next player
+			nextPlayer();
+		}
+});
+
+document.querySelector('.btn-hold').addEventListener('click',function(){
+	// Add CURRENT score to GLOBAL score
+	scores[activePlayer] += roundScore;
+
+	// Update the UI
+	document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+	// Check if player won the game
+	if(scores[activePlayer] >= 100) {
+		document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+		document.querySelector('.dice').style.display = 'none';
+		document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+		document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+	} else{
+		//Next player
+		nextPlayer();
+	}
+
+
+})
+
+document.querySelector('.btn-new').addEventListener('click', function(){
+	
+})
+
+
+function nextPlayer(){
+	//Next player
 			activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
 			roundScore = 0;
 
@@ -53,5 +84,4 @@ document.querySelector('.btn-roll').addEventListener('click', function(){
 			document.querySelector('.player-1-panel').classList.toggle('active');
 
 			document.querySelector('.dice').style.display = 'none';
-		}
-});
+}
