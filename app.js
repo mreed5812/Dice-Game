dc97bcaaab32f73f8b31d1e56c96f85ece559150
Mike Reed <mreed5812@gmail.com>
2018-05-21 22:43:14 -0500

    Added game functionality for 'new game'

--- app.js
+++ app.js
@@ -11,32 +11,22 @@ GAME RULES:
 
 var scores, roundScore, activePlayer, gamePlaying;
 
-scores = [0,0];
-roundScore = 0;
-activePlayer = 0;
-
-
-document.querySelector('.dice').style.display = 'none';
-
-document.getElementById('score-0').textContent = '0';
-document.getElementById('score-1').textContent = '0';
-document.getElementById('current-0').textContent = '0';
-document.getElementById('current-1').textContent = '0';
+init();
 
 
 document.querySelector('.btn-roll').addEventListener('click', function(){
 
-		// 1.Random number
+		// Random number
 
 		var dice = Math.floor(Math.random() * 6) + 1;
 
-		// 2. Display the result
+		// Display the result
 		var diceDOM = document.querySelector('.dice')
 		diceDOM.style.display = 'block';
 		diceDOM.src = 'dice-' + dice + '.png';
 
 
-		// 3. Update the round score only IF the rolled number was not a 1
+		// Update the round score only IF the rolled number was not a 1
 		if (dice !== 1) {
 			//Add score
 			roundScore += dice;
@@ -67,9 +57,6 @@ document.querySelector('.btn-hold').addEventListener('click',function(){
 
 })
 
-document.querySelector('.btn-new').addEventListener('click', function(){
-	
-})
 
 
 function nextPlayer(){
@@ -84,4 +71,27 @@ function nextPlayer(){
 			document.querySelector('.player-1-panel').classList.toggle('active');
 
 			document.querySelector('.dice').style.display = 'none';
+}
+
+document.querySelector('.btn-new').addEventListener('click', init);
+
+
+function init(){
+	scores = [0,0];
+	activePlayer = 0;
+	roundScore = 0;
+
+	document.querySelector('.dice').style.display = 'none';
+
+	document.getElementById('score-0').textContent = '0';
+	document.getElementById('score-1').textContent = '0';
+	document.getElementById('current-0').textContent = '0';
+	document.getElementById('current-1').textContent = '0';
+	document.getElementById('name-0').textContent = 'Player 1';
+	document.getElementById('name-1').textContent = 'Player 2';
+	document.querySelector('.player-0-panel').classList.remove('winner');
+	document.querySelector('.player-1-panel').classList.remove('winner');
+	document.querySelector('.player-0-panel').classList.remove('active');
+	document.querySelector('.player-1-panel').classList.remove('active');
+	document.querySelector('.player-0-panel').classList.add('active');
 }
